a0fbf0b044e99a57ca5ba8fc9838ceb269f487b3
Gebhard Woestemeyer <g@ctr.lc>
2017-02-04 19:57:13 +0100

    exiftool: use DateTimeOriginal instead of CreateDate

--- opt/bin/raw_move.sh
+++ opt/bin/raw_move.sh
@@ -9,4 +9,7 @@ FMT="%Y/%Y%m%d"
 src=$1
 SRC=${src:-.}
 
-exiftool -progress '-Directory<CreateDate' -d ${DST}/${FMT} -r -ext $EXT $SRC
+# exiftool -progress '-Directory<CreateDate' -d ${DST}/${FMT} -r -ext $EXT $SRC
+exiftool -progress '-Directory<DateTimeOriginal' -d ${DST}/${FMT} -r -ext $EXT $SRC
+
+# mts files only have DateTimeOriginal
